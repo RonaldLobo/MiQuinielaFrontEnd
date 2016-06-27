e38bb4e0215544c7758bebdfe4bcf40cf0021396
Ronald Lobo <ronald.lobo@pointclearsolutions.com>
2016-06-27 13:59:52 -0600

    estructura agregada

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miQuinielaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the miQuinielaApp
+ */
+angular.module('miQuinielaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miQuinielaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the miQuinielaApp
+ */
+angular.module('miQuinielaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
