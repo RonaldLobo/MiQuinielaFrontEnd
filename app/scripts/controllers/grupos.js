9697e185bafcf5684981b6ee1d0312252e51b884
Marlon Andre Castro Gonzalez <marlonc@thehangar.cr>
2016-07-20 07:41:12 -0600

    + grupos

--- app/scripts/controllers/grupos.js
+++ app/scripts/controllers/grupos.js
@@ -15,29 +15,26 @@ angular.module('miQuinielaApp')
       'Karma'
     ];
     //get user logged
-    $scope.usuario=auth.loggedUser.id;
-    /*$scope.$watch( function () { return auth.loggedUser; }, function (loggedUser) {
-    	$scope.usuario = loggedUser.name;
-  	}, true);*/
-    var username=$scope.usuario;
-    
+    $scope.usuario=auth.loggedUser.id
     $scope.ordenUsuarios='-puntaje';
     $scope.flecha="Desc";
+	$scope.usuarios={};
+	$scope.grupos={};
+    $scope.displayAddGrupoModal = false;
+    $scope.grupoNuevo={};
+    $scope.grupoNuevo.listaUsuarios=[];
+
+
     $scope.cambiarOrden=function  (argument) {
     	$scope.flecha==="Desc" ? $scope.flecha="Asc": $scope.flecha="Desc";
     	$scope.ordenUsuarios==='-puntaje' ? $scope.ordenUsuarios='puntaje': $scope.ordenUsuarios='-puntaje';
 
     }
-	$scope.usuarios={};
-	$scope.grupos={};
   	var actualizaGrupos = function(){
   		console.log('called 1');
   		var request = $.ajax({
-		  url: "http://localhost/API/index.php/grupos/?userId="+username,
+		  url: "http://localhost/API/index.php/grupos/?userId="+$scope.usuario,
 		  method: "GET",
-		  data: {
-		      format: 'json'
-		   },
 		   dataType: 'json',
 		   success: function(data) {
 		  	$scope.$apply(function() {
@@ -50,14 +47,16 @@ angular.module('miQuinielaApp')
     	  contentType: "application/json; charset=utf-8",
 		});
   	};
+    var cambiaMensaje=function(msg){
+
+			$scope.error=msg;
+
+    };
   	$scope.actualizarLista = function(grupoId){
 
   		var request= $.ajax({
 		  url: "http://localhost/API/index.php/usuarios/?userPoints="+grupoId,
 		  method: "GET",
-		  data: {
-		      format: 'json'
-		   }, 
 		   dataType: 'json',
 		   success: function(data) {
 		  		$scope.$apply(function() {
@@ -69,6 +68,80 @@ angular.module('miQuinielaApp')
 		});
 		      
   	};
+  	$scope.agregarGrupo = function(){
+    	$scope.displayAddGrupoModal = true;
+    };
+    $scope.agregarUsuario=function(){
+    	if($scope.grupoNuevo.user!==""){
+	    	var request= $.ajax({
+			  url: "http://localhost/API/index.php/usuarios/?byUser="+ ($scope.grupoNuevo.user),
+			  method: "GET",
+			   dataType: 'json',
+			   success: function(data) {
+			  		$scope.$apply(function() {
+			  			if(data.usuarios.usuario!=="" && $scope.usuario!==data.usuarios.id && !usuarioRepetido(data.usuarios.usuario)){
+				  			$scope.grupoNuevo.listaUsuarios[$scope.grupoNuevo.listaUsuarios.length]={
+				  				usuario:data.usuarios.usuario,
+				  				id:data.usuarios.id,
+				  				correo:data.usuarios.correo,
+				  				nombre:data.usuarios.nombre+" "+data.usuarios.apellido1
+				  			};
+							cambiaMensaje("");
+				  		}else {cambiaMensaje("Usuario invalido");}
+					$scope.grupoNuevo.user="";
+			  		});
+			  		
+
+			   },
+	    	  contentType: "application/json; charset=utf-8",
+			});
+		}else{
+			cambiaMensaje("Usuario vacío");
+		}
+    };
+    var usuarioRepetido=function(user){
+    	for (var i = $scope.grupoNuevo.listaUsuarios.length - 1; i >= 0; i--) {
+    		if($scope.grupoNuevo.listaUsuarios[i].usuario===user){
+    			return true;
+    		}
+    	};
+    	return false;
+    };
+    $scope.removerUsuario=function(elemento){
+
+    		$scope.grupoNuevo.listaUsuarios.splice(elemento,1);
+    };
+    $scope.crearGrupo=function(){
+    	alert($scope.grupoNuevo.listaUsuarios.length);
+    	
+    	var request= $.ajax({
+			  url: "http://localhost/API/index.php/grupos/",
+			  method: "POST",
+			  data: {
+			      usuario:""
+			   }, 
+			   dataType: 'json',
+			   success: function(data) {
+			   	
+			   },
+	    	  contentType: "application/json; charset=utf-8",
+			});
+
+    	request= $.ajax({
+			  url: "http://localhost/API/index.php/invitaciones/",
+			  method: "POST",
+			  data: {
+			      usuarioGrupo: {
+			      	usuario:""
+			      }
+			   }, 
+			   dataType: 'json',
+			   success: function(data) {
+
+			   },
+	    	  contentType: "application/json; charset=utf-8",
+			});
+    };
   	actualizaGrupos();
     
   });
